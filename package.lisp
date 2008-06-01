523e993b73a125ef1f480c05f14bd4065e38645d
Ramarren <ramarren@gmail.com>
2008-06-01 17:10:45 +0200

    Export emsquare-of

--- package.lisp
+++ package.lisp
@@ -1,5 +1,6 @@
 (defpackage #:opengl-text
   (:use #:cl #:iterate #:alexandria #:vecto #:ffa #:affi)
   (:shadowing-import-from #:alexandria #:rotate)
-  (:export #:draw-gl-string #:opengl-text #:color-of #:font-loader-of #:ensure-characters))
+  (:export #:draw-gl-string #:opengl-text #:color-of #:font-loader-of #:ensure-characters
+	   #:emsquare-of))
 
