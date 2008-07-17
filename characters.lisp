Ramarren/opengl-text

commit feb157b38bc8da2e73af5694964bece811e0a8e6

glyph class encapsulates char tex-coord & cell
This eliminates the character-cells in the opengl-text class.

Ideally CLOS is fast enough to allow for its use in something so
commonly referenced as a glyph.

Also eliminated the sorting of chars in flush-texture, as this seems to
no longer be necessary.

characters.lisp
@@ -1,5 +1,10 @@
 (in-package :opengl-text)
 
+;; each character in character-hash refers to a glyph object
+(defclass glyph ()
+  ((tex-coord :initarg :tex-coord :accessor tex-coord-of)
+   (cell :initarg :cell :accessor cell-of)))
+
 (defun create-char-path (char font em)
   (let ((bb-glyph (zpb-ttf:bounding-box (zpb-ttf:find-glyph char font)))
 	(em (1- em)))
@@ -62,19 +67,19 @@
 				 (list (/ xmax w) (/ ymax h))
 				 (list (/ xmin w) (/ ymax h)))))))))
 
-(defun chars-recoordinate (old-chars character-cells array em)
+(defun chars-recoordinate (old-chars array em)
   (let ((new-character-hash (make-hash-table)))
-    (iter (for old-char in old-chars)
-	  (for old-cell = (gethash old-char character-cells))
-	  (setf (gethash old-char new-character-hash)
-		(transform-cell old-cell array em)))
+    (iter (for (old-char . glyph) in old-chars)
+	  (for old-cell = (gethash old-char old-chars))
+	  (setf (tex-coord-of glyph) (transform-cell (tex-coord-of glyph) array em)
+		(gethash old-char new-character-hash) glyph))
     new-character-hash))
 
-(defun add-new-character (new-char character-hash character-cells cell array em)
-  (setf (gethash new-char character-cells)
-	cell)
+(defun add-new-character (new-char character-hash cell array em)
   (setf (gethash new-char character-hash)
-	(transform-cell cell array em)))
+	(make-instance 'glyph
+		       :tex-coord (transform-cell cell array em)
+		       :cell cell)))
 
 (defgeneric add-char (char gl-text &optional send-texture)
   (:method ((char character) (gl-text opengl-text) &optional (send-texture t))
@@ -89,11 +94,9 @@
 	    (iter (for cell from 0 below (hash-table-count (character-hash-of gl-text)))
 		  (copy-character (texture-of gl-text) cell new-texture cell em))
 	    (setf (character-hash-of gl-text)
-	     (chars-recoordinate (hash-table-keys (character-hash-of gl-text))
-				 (character-cells-of gl-text) new-texture em)))
+	     (chars-recoordinate (character-hash-of gl-text) new-texture em)))
 	  (copy-character (draw-char char gl-text) 0 new-texture (1- new-count) em)
 	  (add-new-character char (character-hash-of gl-text)
-			     (character-cells-of gl-text)
 			     (1- new-count) new-texture em)
 	  (setf (texture-of gl-text) new-texture)
 	  (when send-texture

drawing.lisp
@@ -1,11 +1,9 @@
 (in-package :opengl-text)
 
-(defgeneric get-char-texture-coords (char gl-text)
+(defgeneric get-glyph (char gl-text)
   (:method ((char character) (gl-text opengl-text))
-    (let ((char-coords (gethash char (character-hash-of gl-text))))
-      (if char-coords
-	  char-coords
-	  (add-char char gl-text)))))
+    (or (gethash char (character-hash-of gl-text))
+		      (add-char char gl-text))))
 
 (defun generate-vertices (vertices tex-coords string gl-text kerning depth-shift)
   (iter (with font = (font-loader-of gl-text))
@@ -30,7 +28,7 @@
 	      (aref vertices (+ i 3) 0) (+ k xmin)
 	      (aref vertices (+ i 3) 1) ymax
 	      (aref vertices (+ i 3) 2) (* j depth-shift))
-	(let ((tex-coord (get-char-texture-coords c gl-text)))
+	(let ((tex-coord (tex-coord-of (get-glyph c gl-text))))
 	 (iter (for ii from i to (+ i 3))
 	       (for k from 0)
 	       (setf (aref tex-coords ii 0) (aref tex-coord k 0)

opengl-text.lisp
@@ -22,8 +22,7 @@
    (scale-to-unit :accessor scale-to-unit-of :initform nil)
    (texture :initform nil :accessor texture-of)
    (texture-number :initform nil :accessor texture-number-of)
-   (character-hash :initform (make-hash-table) :accessor character-hash-of)
-   (character-cells :initform (make-hash-table) :accessor character-cells-of)))
+   (character-hash :initform (make-hash-table) :accessor character-hash-of)))
 
 (defmethod initialize-instance :after ((instance opengl-text) &rest initargs)
   (declare (ignore initargs))

texture.lisp
@@ -33,12 +33,10 @@
 (defgeneric flush-texture (gl-text &key new-texture-array)
   (:method ((gl-text opengl-text) &key (new-texture-array nil))
     ;; not the most efficient method
-    (let ((chars (sort (hash-table-keys (character-hash-of gl-text))
-		       #'< :key (rcurry #'gethash (character-cells-of gl-text))))
+    (let ((chars (hash-table-keys (character-hash-of gl-text)))
 	  (em (emsquare-of gl-text)))
       (when chars
 	(setf (character-hash-of gl-text) (make-hash-table))
-	(setf (character-cells-of gl-text) (make-hash-table))
 	(if new-texture-array
 	    (setf (texture-of gl-text) (make-new-texture-array em (length chars)))
 	    (let ((vec (find-original-array (texture-of gl-text))))

commit 841ec69a8fda603d4ceb36fb5b2aa9c21e634d98

fix chars-recoordinate
make call transform-cell with proper first argument
actually iterate through the hash
fixes bugs introduced in feb157b38bc8da2e73af5694964bece811e0a8e6

characters.lisp
@@ -69,9 +69,8 @@
 
 (defun chars-recoordinate (old-chars array em)
   (let ((new-character-hash (make-hash-table)))
-    (iter (for (old-char . glyph) in old-chars)
-	  (for old-cell = (gethash old-char old-chars))
-	  (setf (tex-coord-of glyph) (transform-cell (tex-coord-of glyph) array em)
+    (iter (for (old-char glyph) in-hashtable old-chars)
+	  (setf (tex-coord-of glyph) (transform-cell (cell-of glyph) array em)
 		(gethash old-char new-character-hash) glyph))
     new-character-hash))
 