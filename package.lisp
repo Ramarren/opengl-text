446225098f53ae938797ce899dc4c3fb1708bf14
Ramarren <ramarren@gmail.com>
2008-08-25 15:03:34 +0200

    Update package after ffa/array-operations split

--- package.lisp
+++ package.lisp
@@ -1,5 +1,5 @@
 (defpackage #:opengl-text
-  (:use #:cl #:iterate #:alexandria #:ffa)
+  (:use #:cl #:iterate #:alexandria #:ffa #:array-operations)
   (:shadowing-import-from #:alexandria #:rotate)
   (:export #:draw-gl-string #:opengl-text #:color-of #:font-loader-of #:ensure-characters
            #:emsquare-of #:*auto-extend-buffers*))
