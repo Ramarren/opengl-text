b13d2a61de9daf5352a4ad8d55ebfc35037f74e1
Ramarren <ramarren@gmail.com>
2008-11-07 12:45:33 +0100

    Export more symbols

--- package.lisp
+++ package.lisp
@@ -3,4 +3,6 @@
   (:shadowing-import-from #:alexandria #:rotate)
   (:export #:draw-gl-string #:opengl-text #:font-loader-of #:ensure-characters
            #:emsquare-of #:*auto-extend-buffers*
-           #:polygonal-opengl-text))
+           #:polygonal-opengl-text
+           #:bezier-distance-tolerance-of
+           #:bezier-angle-tolerance-of))
