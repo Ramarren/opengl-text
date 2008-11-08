ab2b5a6b102f189b7cbbf8d262b3096c07d38e12
Ramarren <ramarren@gmail.com>
2008-11-08 12:01:05 +0100

    Export mipmap-opengl-text

--- package.lisp
+++ package.lisp
@@ -1,7 +1,7 @@
 (defpackage #:opengl-text
   (:use #:cl #:iterate #:alexandria #:ffa #:array-operations)
   (:shadowing-import-from #:alexandria #:rotate)
-  (:export #:draw-gl-string #:opengl-text #:font-loader-of #:ensure-characters
+  (:export #:draw-gl-string #:opengl-text #:mipmap-opengl-text #:font-loader-of #:ensure-characters
            #:emsquare-of #:*auto-extend-buffers*
            #:polygonal-opengl-text
            #:bezier-distance-tolerance-of
