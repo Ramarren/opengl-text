18121e5977b276cb1039486169fcbcd9bf9dff8c
Ramarren <ramarren@gmail.com>
2009-01-09 18:32:44 +0100

    Update packages.

---
+++ drawing/package.lisp
@@ -0,0 +1,3 @@
+(defpackage #:opengl-text-vector
+  (:use #:cl #:iterate #:alexandria)
+  (:export #:make-glyph-vector-factory))
--- package.lisp
+++ package.lisp
@@ -1,10 +1,7 @@
 (defpackage #:opengl-text
   (:use #:cl #:iterate #:alexandria #:ffa #:array-operations)
   (:shadowing-import-from #:alexandria #:rotate)
-  (:export #:draw-gl-string  #:font-loader-of #:ensure-characters
-           #:opengl-text #:mipmap-opengl-text #:packed-mipmap-opengl-text
-           #:emsquare-of #:*auto-extend-buffers*
-           #:polygonal-opengl-text
-           #:bezier-distance-tolerance-of
-           #:bezier-angle-tolerance-of
-           #:line-width-of))
+  (:export #:draw-gl-string  #:ensure-characters
+           #:textured-opengl-text #:mipmap-opengl-text
+           #:emsquare-of #:*auto-extend-buffers*))
+
